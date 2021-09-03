55b6ed51cdc5c22920ff8cd8835d4be953ad0a74
Zaid <iamzaidsheikh@gmail.com>
2021-09-03 14:43:10 +0530

    Added razorpay checkout

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,4 +1,4 @@
-import 'package:charted/widgets/wallet_details_card.dart';
+import '../widgets/wallet_details_card.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
--- lib/widgets/wallet_details_card.dart
+++ lib/widgets/wallet_details_card.dart
@@ -1,19 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 
-class WalletDetailsCard extends StatelessWidget {
+class WalletDetailsCard extends StatefulWidget {
   final theme;
   final size;
   final String balance;
 
   const WalletDetailsCard(this.theme, this.size, this.balance);
 
+  @override
+  _WalletDetailsCardState createState() => _WalletDetailsCardState();
+}
+
+class _WalletDetailsCardState extends State<WalletDetailsCard> {
+  late Razorpay _razorpay;
+
+  @override
+  void initState() {
+    super.initState();
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+  }
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    // Do something when payment succeeds
+  }
+  void _handlePaymentError(PaymentFailureResponse response) {
+    // Do something when payment fails
+  }
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    // Do something when an external wallet is selected
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _razorpay.clear();
+  }
+
   Widget build(BuildContext context) {
     return Container(
-      height: size.height * 0.28,
-      width: size.width * 0.9,
+      height: widget.size.height * 0.28,
+      width: widget.size.width * 0.9,
       decoration: BoxDecoration(
-        color: theme.accentColor,
+        color: widget.theme.accentColor,
         borderRadius: BorderRadius.all(Radius.circular(20.0)),
       ),
       child: Column(
@@ -32,7 +65,7 @@ class WalletDetailsCard extends StatelessWidget {
 
           //Amount
           Text(
-            '\$' + balance,
+            '\$' + widget.balance,
             style: GoogleFonts.inter(
               color: Colors.white,
               fontSize: 28,
@@ -42,22 +75,46 @@ class WalletDetailsCard extends StatelessWidget {
 
           //Icon Buttons
           Container(
-            width: size.width * 0.75,
+            width: widget.size.width * 0.75,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 //Add Button
-                _walletCardIconButton(
-                    theme, size, 'Add', Icons.arrow_downward_rounded),
+                _walletCardIconButton(widget.theme, widget.size, 'Add',
+                    Icons.arrow_downward_rounded, () async {
+                  print('Add button tapped');
+                  var options = {
+                    'key': 'rzp_test_QmzLview4RmIzC',
+                    'amount': 50000, //in the smallest currency sub-unit.
+                    'name': 'charted',
+                    'order_id':
+                        'order_HswCHDdv3kfNkh', // Generate order_id using Orders API
+                    'prefill': {
+                      'contact': '9123456789',
+                      'email': 'gaurav.kumar@example.com'
+                    }
+                  };
+
+                  try {
+                    _razorpay.open(options);
+                  } catch (e) {
+                    debugPrint('Error: e');
+                  }
+                }),
 
                 //Withdraw Button
-                _walletCardIconButton(
-                    theme, size, 'Withdraw', Icons.arrow_upward_rounded),
+                _walletCardIconButton(widget.theme, widget.size, 'Withdraw',
+                    Icons.arrow_upward_rounded, () {
+                  print('Withdraw button tapped');
+                }),
 
                 //History Button
                 _walletCardIconButton(
-                    theme, size, 'More', Icons.more_horiz_rounded),
+                    widget.theme, widget.size, 'More', Icons.more_horiz_rounded,
+                    () {
+                  print('More button tapped');
+                }),
               ],
             ),
           ),
@@ -71,6 +128,7 @@ class WalletDetailsCard extends StatelessWidget {
     final size,
     String label,
     IconData icon,
+    final Function() _action,
   ) {
     return Container(
       child: Column(
@@ -79,9 +137,7 @@ class WalletDetailsCard extends StatelessWidget {
         children: [
           //Icon
           GestureDetector(
-            onTap: () {
-              print(label + ' button pressed');
-            },
+            onTap: _action,
             child: Container(
               decoration: BoxDecoration(
                 color: theme.primaryColorDark,
